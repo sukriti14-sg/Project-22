42859f3b9904e2899db66e997fb069c7fa12d7e8
sukriti14-sg <76900300+sukriti14-sg@users.noreply.github.com>
2021-04-30 21:12:55 +0530

    Add files via upload

---
+++ sydhjsTemPlate-main/sketch.js
@@ -0,0 +1,73 @@
+var starImg,bgImg;
+var star, starBody;
+var fairy, fairyImg
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyImg = loadImage("images/fairy.png");
+	fairySound = loadSound("sound/JoyMusic.mp3")
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	fairySound.play();
+
+	//create fairy sprite and add animation for fairy
+	fairy = createsprite()
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  if(star.y > 470 && starBody.position.y > 470 ){
+	  Matter.body.setStatic(starBody,true);
+  }
+  
+
+  drawSprites();
+
+
+}
+
+function keyPressed() {
+
+	if(keyCode === RIGHT_ARROW){
+	 fairy.x = fairy.x + 20;
+    }
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//writw code to move fairy left and right
+	
+}
